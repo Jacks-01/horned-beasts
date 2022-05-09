23a2198512e1e3b7afca0f4374a92742a483f5d6
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-09 14:46:39 -0500

    finished react app

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Component } from "react";
+import HornedBeasts from "./Horned-Beasts";
+
+
+class Main extends Component {
+    render() { 
+        return (
+            <main>
+                <p>Hello from Main</p>
+                <HornedBeasts/>
+            </main>
+        );
+    }
+}
+ 
+export default Main;
